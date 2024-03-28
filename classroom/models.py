bf03621c7541408fc5fb0898cb9ba593610eafae
DiegoH3nr1que <diegohenrique123012@gmail.com>
2024-03-28 20:02:43 -0300

    feat(): implementation model of project
    creating the models file to start the project

--- classroom/models.py
+++ classroom/models.py
@@ -1,3 +1,24 @@
+from typing import Any
 from django.db import models
 
-# Create your models here.
+class ClassromEntity:
+
+    def __init__(self, id,  name, date, descricao,disciplina= [
+        "LP", "ML", "IOT", "BIGDATA", "WEB", "MOBILE"
+    ]) -> None:
+        self.id = id
+        self.name = name
+        self.date = date
+        self.descricao = descricao
+        self.disciplina = disciplina
+
+    def __str__(self) -> str:
+        return (f"Classroom <{self.name}")
+    
+    def __getattribute__(self, __name: str) -> Any:
+        if (__name=='date'):
+            return object.__getattribute__(self, __name).strftime("%d/%m/%Y %H:%M:%S")
+        else:
+            return object.__getattribute__(self,__name)
+
+        
